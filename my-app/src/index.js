ccb807192712e128c3d95f47e479ed61ffbeb76c
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-07-10 00:15:38 +0900

    🐛 파일명 에러 수정

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotifacationList';
+import NotificationList from './chapter_06/NotificationList';
 
 ReactDOM.render(
   <React.StrictMode>
